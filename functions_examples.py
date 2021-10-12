3b8d113f6cac88303143d7a4cd923740c710af52
Soyokaze-42 <Soyokaze-42@users.noreply.github.com>
2021-10-12 11:07:04 -0500

    Create functions_examples.py

---
+++ functions_examples.py
@@ -0,0 +1,15 @@
+# function definition minimum
+def function_name():
+  pass # your code goes in this codeblock
+       # return None is implied here
+
+# function call minimum
+function_name() #this does nothing, but it runs
+
+# function with parameters
+def func_with_param(param1, param2):
+  return_value = str(param1) + ' ' + str(param2) # string concatination
+  return return_value
+
+# function call with hardcoded values passed as arguments
+new_string = func_with_param('Steve', 'Cheetoes')
